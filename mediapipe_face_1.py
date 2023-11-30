e10e60f3729e66d32176cd4debb7096e8037b8a8
iTuam7 <123919001+iTuam0@users.noreply.github.com>
2023-11-30 23:15:43 +0500

    Update mediapipe_face_1.py
    - Создал простой графический интерфейс Tkinter с окном просмотра, кнопкой запуска и строкой состояния.
    - Интегрировал потоковое видео с помощью OpenCV и отображал кадры на холсте Tkinter.
    - Добавлена кнопка "Старт" для запуска видеопотока.
    - Реализована функция, запускающая видеопоток только в том случае, если он еще не запущен.
    - Привязка кнопки "Старт" к событию щелчка левой кнопкой мыши.
    - Предоставлена строка состояния с информацией о бета-режиме.

--- mediapipe_face_1.py
+++ mediapipe_face_1.py
@@ -3,63 +3,49 @@ import mediapipe as mp
 import cv2
 from PIL import Image, ImageTk
 
-#Start;
-def main_window():
-      #Basic form;
-      main_window = tk.Tk()
-      main_window.title("iFrame")
+def create_gui():
+    main_window_instance = tk.Tk()
+    main_window_instance.title("iFrame")
+    main_window_instance.geometry("1000x500")
+    main_window_instance.resizable(False, False)
 
-      #Form size;
-      main_window.geometry("1000x500")
-      main_window.resizable(False, False)
+    label_1 = tk.Label(main_window_instance, text=" Viewing window:", fg="red")
+    label_1.pack(anchor="w")
 
-      #Label;
-      label_1 = tk.Label(main_window, text=" Viewing window:", fg="red")
-      label_1.pack(anchor="w")
+    canvas_form = tk.Canvas(main_window_instance, width="750", height="450", bd=1, relief="raised", highlightbackground="blue", highlightthickness=2, bg="gray")
+    canvas_form.pack(anchor="w", padx=5)
 
-      #Convas;
-      canvas_form = tk.Canvas(main_window, width="750", height="450", bd=1, relief="raised", highlightbackground="blue", highlightthickness=2)
-      canvas_form.pack(anchor="w", padx=5)
+    button_start = tk.Button(main_window_instance, text="Start", command=start_video_flow)
+    button_start.pack()
 
-      #Create a url with the id from the camera;
-      url = "http://192.168.1.127:4747/video"
+    status_bar = tk.Label(main_window_instance, bd=1, text=" The program is in beta mode. In general it works, but there are drawbacks.", relief=tk.SUNKEN, anchor=tk.W, fg="blue")
+    status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
-      #Initialization of mediapipe modules;
-      m_faceD = mp.solutions.face_detection
-      m_draw = mp.solutions.drawing_utils
-      f_detection = m_faceD.FaceDetection()
+    return main_window_instance, canvas_form
 
-      #Open the stream coming from the url;
-      flow_pictures = cv2.VideoCapture(url)
+def start_video_flow():
+    global flow_pictures
+    if 'flow_pictures' not in globals() or not flow_pictures.isOpened():
+        flow_pictures = cv2.VideoCapture(url)
+        update()
 
-      #Status bar;
-      status_bar = tk.Label(main_window, bd=1, text=" The program is in beta mode. In general it works, but there are drawbacks.", relief=tk.SUNKEN, anchor=tk.W, fg="blue")
-      status_bar.pack(side = tk.BOTTOM, fill=tk.X)
+def click_mouse(event):
+    start_video_flow()
 
-      #We'll spam you with updates;
-      def update():
-            #Reading a stream;
-            ret, frame = flow_pictures.read()
+def update():
+    if 'flow_pictures' in globals() and flow_pictures.isOpened():
+        ret, frame = flow_pictures.read()
+        format_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        format_rgb = cv2.resize(format_rgb, (750, 450))
+        images = Image.fromarray(format_rgb)
+        images_tkinter = ImageTk.PhotoImage(image=images)
 
-            #Convert in from Tkinter-format;
-            format_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            format_rgb = cv2.resize(format_rgb, (750, 450))
-            images = Image.fromarray(format_rgb)
-            images_tkinter_F = ImageTk.PhotoImage(image=images)
+        canvas_form.img = images_tkinter
+        canvas_form.create_image(canvas_form.winfo_width() // 2, canvas_form.winfo_height() // 2, anchor=tk.CENTER, image=images_tkinter)
 
-            #Displaying an image on Canvas;
-            images = Image.fromarray(format_rgb)
-            images_tkinter_F = ImageTk.PhotoImage(image=images)
+        main_window_instance.after(10, update)
 
-            canvas_form.img = images_tkinter_F
-            canvas_form.create_image(canvas_form.winfo_width() // 2, canvas_form.winfo_height() // 2, anchor=tk.CENTER, image=images_tkinter_F)
-            
-            #Update main_window;
-            main_window.after(10, update)
-
-      update()
-      
-      #End of main_form cycle;
-      main_window.mainloop()
-#View;
-main_window()
+url = "http://192.168.1.127:4747/video"
+main_window_instance, canvas_form = create_gui()
+canvas_form.bind("<Button-1>", click_mouse)
+main_window_instance.mainloop()
